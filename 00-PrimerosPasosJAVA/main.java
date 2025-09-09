135ea22bd2d70a78f00db229359b1f3e98fb4534
Ayrton Vargas <ayrtonvargas17@gmail.com>
2025-09-09 02:07:18 -0300

    primeros pasos

--- 00-PrimerosPasosJAVA/main.java
+++ 00-PrimerosPasosJAVA/main.java
@@ -1,5 +1,20 @@
-public class Main {
+public class main {
     public static void main(String[] args) {
+        // Hola Mundo
         System.out.println("Hola Mundo");
+
+        //Crear una variable
+        int a = 10;
+        a += 5;
+        String MyString = "Soy Ayrton ";
+        System.out.println(MyString);
+        System.out.println(a);
+
+        //Tipos de datos
+        Double myDouble = 3.14;
+        System.out.println(myDouble);
+
+        
+
     }
 }
